b0877c348f58fd3bfd03825e893ef21be96017f2
Joanna <j.k.szczesna@gmail.com>
2024-12-16 17:40:46 +0100

    initial

---
+++ src/test/java/pl/joannaszczesna/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.joannaszczesna.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
